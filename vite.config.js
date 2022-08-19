41176d34a6b6a21ef8d4c8f3475915429bef30d1
jzapata <julianandreszb@gmail.com>
2022-08-19 03:50:22 -0500

    vite before build and deploy.sh

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/<REPO>/",
+  base: "/rickandmorty-vue3-compositionapi/",
   plugins: [vue()],
   resolve: {
     alias: {
